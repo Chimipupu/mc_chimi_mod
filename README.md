16517041c1633c1d3ceb8ee7464245ef9fe53764
Chimipupu <chiminyanyan@gmail.com>
2024-10-22 15:01:54 +0900

    ベース ... マイクラでMODが読み込みできる状態

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mc_chimi_mod
+# mc_chimi_mod (開発中)
 Minecraft Chimi's(ちみ) Mod🥳
+
+ ![mod_read_img](doc/mod_read.png)
+
+## 開発環境(MDK)
+ - MDK ... MinForge 1.21.1
---
+++ src/main/java/com/chimi/mc_chimi_mod/ChimiMod.java
@@ -0,0 +1,45 @@
+package com.chimi.mc_chimi_mod;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ChimiMod.MOD_ID)
+public class ChimiMod {
+    public static final String MOD_ID = "mc_chimi_mod";
+
+    public ChimiMod(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // TODO
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // TODO
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // TODO
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // TODO
+        }
+    }
+}
